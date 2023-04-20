b6685e5628b2f1bd227468ed8c268dc2ee10de1b
Fiona-alice <120317574+Fiona-alice@users.noreply.github.com>
2023-04-20 15:57:50 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
 //your JS code here. If required.
-function findFirstNonRepeatingChar() {
-  const inputString = prompt("Enter a string:");
-
-  if (!inputString) {
-    alert("No input provided.");
-    return null;
-  }
-
-  const charCount = new Map();
-
-  // Count occurrences of each character in the input string
-  for (let char of inputString) {
-    if (charCount.has(char)) {
-      charCount.set(char, charCount.get(char) + 1);
-    } else {
-      charCount.set(char, 1);
-    }
-  }
-
-  // Find the first non-repeating character
-  for (let char of inputString) {
-    if (charCount.get(char) === 1) {
-      alert(`The first non-repeating character is: ${char}`);
-      return char;
-    }
-  }
-
-  // If no non-repeating character found
-  alert("No non-repeating character found.");
-  return null;
-}
-
-findFirstNonRepeatingChar();
+function nonRepeat(str){
+	var output;
+var newString="";
+	for(let i=0;i<str.length();i++){
+		for(let j=i+1;j<str.length();j++){
+			if(i!=j && str.charAt(i)!=str.charAt(j)){
+				output=str.charAt(i);
+			}
+		}
+		var input=prompt("Enter a string",str);
+		nonRepeat(input);
+		alert(output);

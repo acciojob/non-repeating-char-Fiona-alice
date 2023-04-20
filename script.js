d04d36cdc0e3f01b7eaada4d88eb6e55a6f95d66
Fiona-alice <120317574+Fiona-alice@users.noreply.github.com>
2023-04-20 16:27:06 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 //your JS code here. If required.
-var str=prompt("Enter a string");
-let splitedArr=str.split("");
-console.log(splitedArr);
-var newEle="";
-var i=0;
-for (let i = 0; i < splitedArr.length; i++) {
-	for (let j = 0; j < splitedArr.length; j++){
-		if(splitedArr[i]==splitedArr[j]&&i!=j){
-			console.log(splitedArr[j]);
-			break;
-		}else{
-			newEle=splitedArr[j]
-		}
-		}
-	}
-alert(newEle);
+function firstNonRepeatedCharacter(str) {
+  // create an object to store the count of each character
+  const charCount = {};
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    charCount[char] = charCount[char] ? charCount[char] + 1 : 1;
+  }
+
+  // loop through the string again and return the first non-repeated character
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  // if there is no non-repeated character, return null
+  return null; 
+}
+
+const input = prompt("Enter a string:");
+const result = firstNonRepeatedCharacter(input);
+alert(result ? result : null);
